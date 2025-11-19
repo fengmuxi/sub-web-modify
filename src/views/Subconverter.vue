38e1b37922e2941d60be457238a12e2934a9f271
xiao <1809489542@qq.com>
2025-11-19 15:59:58 +0800

    修改后端接口地址

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "阿里云自供后端": "http://clash.api.dwzynj.top",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
